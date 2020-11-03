380c23f72bc9d3bbd7492d4b6dc39c519b8e7e5d
SandeepMaithani <sandeep1137@gmail.com>
2020-11-04 00:52:10 +0530

    Unique Binary Search Trees

--- Binary Search Tree/Unique BST's.cpp
+++ Binary Search Tree/Unique BST's.cpp
@@ -4,7 +4,7 @@ Method 1 : Recursion + Memoization                              Verdict : Accept
 
 Complexity Analysis:
 
-Time complexity : O(N*N), .
+Time complexity : O(N*N), Since we will consider each node as root node it will require O(N) time to memoize data so O(N*N).
 
 Space Complexity : O(N), we need extra space to store values after inorder traversal.
 
@@ -14,11 +14,56 @@ Space Complexity : O(N), we need extra space to store values after inorder trave
 
 /*
 
+Intution : -
+
+For a current N, each node 'i' till 'n' can become a root,
+and the elements smaller than 'i' are left subtree nodes and greater than 'i' are part of right subtree.
+Both left and right subtree nodes then again be arranged amongst themselves
+
+*/
+
+
+class Solution {
+public:
+	int numTreesHelper(int N, vector<int>& dp) {
+
+		if (N <= 1) {
+			return 1;
+		}
+
+		if (dp[N] != -1) {
+			return dp[N];
+		}
+
+		int countStructure = 0;
+
+		for (int i = 1; i <= N; i++) {
+
+			int leftCount = numTreesHelper(i - 1, dp);
+			int rightCount = numTreesHelper(N - i, dp);
+
+			countStructure += leftCount * rightCount;
+		}
+
+		return (dp[N] = countStructure);
+	}
+
+	int numTrees(int N) {
+		vector<int> dp(N + 1, -1);
+		return numTreesHelper(N, dp);
+	}
+};
+
+
+
+
+/*
+
 Method 2 : Recursive Solution                            Verdict : TLE
 
 Complexity Analysis:
 
-Time complexity : O(N!), where N is no of nodes in the tree.
+Time complexity : O(2^N), where N is no of nodes in the tree.
 
 Space Complexity : O(1), No extra space required.
 
@@ -33,7 +78,11 @@ int numTrees(int N) {
 	int countStructure = 0;
 
 	for (int i = 1; i <= N; i++) {
-		countStructure += (numTrees(i - 1) * numTrees(N - i));
+
+		int leftCount = numTrees(i - 1);
+		int rightCount = numTrees(N - i);
+
+		countStructure += leftCount * rightCount;
 	}
 
 	return countStructure;

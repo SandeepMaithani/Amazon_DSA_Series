2fb3fa3b5454298f14bf0bb9435f2650fa7bf23c
SandeepMaithani <sandeep1137@gmail.com>
2020-11-19 02:37:04 +0530

    Merge K sorted linked lists

--- Linked List/Merge K sorted linked lists.cpp
+++ Linked List/Merge K sorted linked lists.cpp
@@ -1,5 +1,80 @@
 /*
 
+Method 1 : using priority queue                              Verdict : Accepted
+
+Complexity Analysis:
+
+Time complexity : O(N*log(k)) where k is the number of linked lists.
+
+The comparison cost will be reduced to O(log(k)) for every pop and insertion to priority queue. But finding the node
+with the smallest value just costs O(1) time.There are N nodes in the final linked list.
+
+Space complexity : O(k) The code above present applies in-place method which cost O(1) space. And the priority queue
+(often implemented with heaps) costs O(k) space (it's far less than N in most situations).
+
+*/
+
+
+/*
+
+Intution :-
+
+Key idea here is we will store first node of all the linked list in a min heap and then merge the minimum element from heap
+to the merge list and put the next node of selected list in the min Heap by this we will have atmost K element at any
+point of time in our min heap and we will merge list simultaneously.
+
+
+*/
+
+
+class Solution {
+public:
+    ListNode* mergeHelper(vector<ListNode*>& lists) {
+        priority_queue<pair<int, ListNode*>, vector<pair<int, ListNode*>>, greater<pair<int, ListNode*>>>minHeap;
+        ListNode* dummy = new ListNode(-1);
+        ListNode* merger = dummy;
+
+        for (int i = 0; i < lists.size(); i++) {
+            if (lists[i] != NULL) {
+                minHeap.push({lists[i] -> val, lists[i]});
+            }
+        }
+
+        while (minHeap.size() != 0) {
+            pair<int , ListNode*>currNode = minHeap.top();
+            minHeap.pop();
+
+            merger -> next = currNode.second;
+            merger = merger -> next;
+
+            ListNode* nextNode = currNode.second -> next;
+
+            if (nextNode != NULL) {
+                minHeap.push({nextNode -> val, nextNode});
+            }
+        }
+
+        return dummy -> next;
+    }
+
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        if (lists.size() == 0) {
+            return NULL;
+        }
+
+        if (lists.size() == 1) {
+            return lists[0];
+        }
+
+        return mergeHelper(lists);
+
+    }
+};
+
+
+
+/*
+
 Method 2 : using merge two linked List                            Verdict : Accepted
 
 Complexity Analysis:

10ad4b0465dd865a0222f3fa1c3f5cabda2dcbe2
SandeepMaithani <sandeep1137@gmail.com>
2020-11-27 21:20:29 +0530

    Given a linked list, reverse alternate nodes and append at the end

--- Linked List/Given a linked list, reverse alternate nodes and append at the end.cpp
+++ Linked List/Given a linked list, reverse alternate nodes and append at the end.cpp
@@ -1,57 +1,80 @@
 /*
 
-Complexity Analysis :-
+Complexity analysis :-
 
-Time Complexity: O(N)
-Auxiliary Space: O(1)
+Time complexity : O(N). Where N is the No of Nodes in the Linked list, and we will any node atmost twice.
+
+Space complexity : O(1). We will use constant space to maintain even and odd list.
 
 */
 
-Node *reverseHelper(Node *head) {
-    if (head == NULL) {
-        return NULL;
-    }
 
-    Node *prev = NULL;
-    Node *mover = head;
+/*
 
-    while (mover != NULL) {
-        Node *next = mover -> next;
-        mover -> next = prev;
-        prev = mover;
+Intution :-
 
-        mover = next;
-    }
+1. The idea is to maintain two linked lists, one list of all odd positioned nodes and other list of all even positioned nodes .
 
-    return prev;
+2. Reverse the Linked list with even positioned nodes.
+
+3. Append the reversed list to the end of Linked List with odd positioned nodes.
+
+
+*/
+
+
+Node* reverseHelper(Node *head) {
+	if (head == NULL) {
+		return head;
+	}
+
+	Node *prev = NULL;
+	Node *mover = head;
+
+	while (mover != NULL) {
+		Node *next = mover -> next;
+		mover -> next = prev;
+		prev = mover;
+
+		mover = next;
+	}
+
+	return prev;
 }
+
 
 void rearrange(struct Node *odd)
 {
 
-    if (odd == NULL || odd -> next == NULL) {
-        return;
-    }
+	if (odd == NULL || odd -> next == NULL || odd -> next -> next == NULL) {
+		return;
+	}
 
-    Node *prev = odd;
-    int numInfo = 1;
+	Node *mover = odd;
+	int numInfo = 1;
 
-    Node *dummy = new Node(-1);
-    Node *dummyHelper = dummy;
+	Node *oddList = new Node(-1);
+	Node *oddHelper = oddList;
 
-    while (odd) {
-        if (numInfo % 2 == 0) {
-            dummyHelper -> next = odd;
-            dummyHelper = dummyHelper -> next;
-            prev -> next = odd -> next;
-        }
-        else {
-            prev = odd;
-        }
-        odd = odd -> next;
-        numInfo++;
-    }
+	Node *evenList = new Node(-1);
+	Node *evenHelper = evenList;
 
-    dummy -> next = reverseHelper(dummy -> next);
-    prev -> next = dummy -> next;
+	while (mover != NULL) {
+		if (numInfo % 2 == 0) {
+			evenHelper -> next = mover;
+			evenHelper = evenHelper -> next;
+		}
+		else {
+			oddHelper -> next = mover;
+			oddHelper = oddHelper -> next;
+		}
+
+		mover = mover -> next;
+		numInfo++;
+	}
+
+	evenHelper -> next = NULL;
+
+	evenList -> next = reverseHelper(evenList -> next);
+	oddHelper -> next = evenList -> next;
 }

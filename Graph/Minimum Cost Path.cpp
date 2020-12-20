78de30828ce4d7514240e932b7ccdae6d418154c
SandeepMaithani <sandeep1137@gmail.com>
2020-12-21 01:48:21 +0530

    Minimum Cost Path

--- Graph/Minimum Cost Path.cpp
+++ Graph/Minimum Cost Path.cpp
@@ -1,13 +1,90 @@
+/*
+
+Method 1 :- using Set for implementation of Dijkstra algo                   Verdict :- Accepted
+
+Complexity Analysis :-
+
+Time Complexity : O(N * N * log N). We will be doing a total of N*N insertion and atmost N element will be there in Set at any point of time.
+
+Space Complexity : O(N*N). We need extra memory for the set and distance matrix which will cost O(N*N) memory.
+
+*/
+
 
 /*
 
-Method 2 :- using minHeap implementation Dijkstra algo                   Verdict :- TLE
+Note :- It is not possible to solve this problem using dynamic programming similar to minimum cost problem (where right and down move are allowed)
+because here current state depends not only on right and bottom cells but also on left and upper cells.
+
+Intution :-
+
+Key idea here is to solve this problem using dijkstra’s algorithm. We will find the shortest path from source to target in term of cost, Each cell
+of grid represents a vertex and neighbor cells adjacent vertices. We do not make an explicit graph from these cells instead we will use matrix as it is
+in our dijkstra’s algorithm and maintain a distance matrix seprately and update the shortest cost(distance) from source to each node(cell) in that matrix.
+
+
+*/
+
+bool isSafe(int x, int y, int n) {
+    if (x < 0 || y < 0 || x >= n || y >= n) {
+        return false;
+    }
+
+    return true;
+}
+
+int shortest(vector<vector<int> > &grid, int n) {
+
+    int distance[1001][1001];
+
+    for (int i = 0; i < 1001; i++) {
+        for (int j = 0; j < 1001; j++) {
+            distance[i][j] = INT_MAX;
+        }
+    }
+
+    set<pair<int, pair<int, int>>>hashSet;
+
+    distance[0][0] = grid[0][0];
+    hashSet.insert({distance[0][0], {0, 0}});
+
+    int dx[] = {0, 0, -1, 1};
+    int dy[] = { -1, 1, 0, 0};
+
+    while (!hashSet.empty()) {
+        pair<int, pair<int, int>> curVertex = *(hashSet.begin());
+        int curCost = curVertex.first;
+        int x = curVertex.second.first;
+        int y = curVertex.second.second;
+
+        hashSet.erase(hashSet.begin());
+
+        for (int i = 0; i < 4; i++) {
+            int newX = x + dx[i];
+            int newY = y + dy[i];
+
+            if (isSafe(newX, newY, n) && (distance[newX][newY] > curCost + grid[newX][newY])) {
+                distance[newX][newY] = curCost + grid[newX][newY];
+                hashSet.insert({distance[newX][newY], {newX, newY}});
+            }
+        }
+
+    }
+
+    return distance[n - 1][n - 1];
+}
+
+
+
+/*
+
+Method 2 :- using minHeap for implementation of Dijkstra algo                   Verdict :- TLE
 
 Complexity Analysis :-
 
 Time Complexity : O(N * N * log N). We will be doing a total of N*N insertion and atmost N element will be there in min heap at any point of time.
 
-Space Complexity : O(N*N). We need extra memory for the visited array and min heap which will cost O(V) memory.
+Space Complexity : O(N*N). We need extra memory for the visited array and min heap which will cost O(N*N) memory.
 
 */
 

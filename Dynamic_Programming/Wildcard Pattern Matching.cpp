60f03d8ac0b3e9721f0fb747a0137edc993faaed
SandeepMaithani <sandeep1137@gmail.com>
2021-01-02 21:09:46 +0530

    Wildcard Pattern Matching

--- Dynamic_Programming/Wildcard Pattern Matching.cpp
+++ Dynamic_Programming/Wildcard Pattern Matching.cpp
@@ -2,11 +2,37 @@
 
 Complexity Analysis :-
 
-Time Complexity: O(N * M).
+Time Complexity: O(N * M). After each cell of N * M DP cell is filled function call will take unique time so overall time
+complexity will be O(N * M) to fill the dp matrix.
 
-Space Complexity : O(N * M).
+Space Complexity : O(N * M). Space complexity will be O(N * M) which will be taken be dp matrix.
 
 */
+
+
+/*
+
+Intution :-
+
+Case 1: The character is ‘*’ Here two cases arise
+
+  a). We can ignore ‘*’ character and move to next character in the Pattern.
+  b). ‘*’ character matches with one or more characters in Text. Here we will move to next character in the string.
+
+Case 2: The character is ‘?’
+
+  We can ignore current character in Text and move to next character in the Pattern and Text.
+
+Case 3: The character is not a wildcard character
+
+  If current character in Text matches with current character in Pattern, we move to next character in the Pattern and Text.
+  If they do not match, wildcard pattern and Text do not match.
+
+We can use Dynamic Programming to solve this problem –
+  Let dp[i][j] is true if first i characters in given string matches the first j characters of pattern.
+
+*/
+
 
 bool LCS(string &pattern, string &word, vector<vector<int>>&dp, int n, int m) {
 
